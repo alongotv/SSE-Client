e9b7e7bcc432f06e96d989564ee5d21bb292910a
Vladimir Vetrov <pdastore1@gmail.com>
2022-02-08 14:09:52 +0300

    Add sealed class SseMessage

---
+++ app/src/main/java/com/alongo/screenovatetest/data/networking/SseMessage.kt
@@ -0,0 +1,8 @@
+package com.alongo.screenovatetest.data.networking
+
+sealed class SseMessage {
+    object Open : SseMessage()
+    object Closed : SseMessage()
+    data class Message(val data: String, val type: String?) : SseMessage()
+    data class Failure(val error: Throwable) : SseMessage()
+}
