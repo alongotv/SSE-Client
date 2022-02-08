39ea3dd64f4c152a4133531b33d35fd1210fe293
Vladimir Vetrov <pdastore1@gmail.com>
2022-02-08 15:55:04 +0300

    Update ViewModel

--- app/src/main/java/com/alongo/screenovatetest/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/alongo/screenovatetest/presentation/main/MainViewModel.kt
@@ -6,24 +6,34 @@ import com.alongo.screenovatetest.data.networking.payment.PaymentApi
 import com.alongo.screenovatetest.domain.entity.Payment
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @HiltViewModel
 class MainViewModel @Inject constructor(private val paymentApi: PaymentApi): ViewModel() {
 
-    var payments: List<Payment> = emptyList()
+    var payments: MutableList<Payment> = mutableListOf()
 
     fun loadPayments() {
         subscribeToPayments()
-        payments = listOf(Payment("", "", 10.0), Payment("", "", 20.0), Payment("", "", 30.0))
+        payments = mutableListOf(Payment("", "", 10.0), Payment("", "", 20.0), Payment("", "", 30.0))
     }
 
-    fun subscribeToPayments() {
+    private fun subscribeToPayments() {
         viewModelScope.launch {
-            paymentApi.getPayments().collect {
-                println(it)
+            while (isActive) {
+                paymentApi.getPayments().collect {
+                    payments.add(it)
+                    Timber.i(it.toString())
+                }
+                delay(paymentLoadingDelayMillis)
             }
         }
+    }
+    companion object {
+        const val paymentLoadingDelayMillis = 5000L
     }
 }
