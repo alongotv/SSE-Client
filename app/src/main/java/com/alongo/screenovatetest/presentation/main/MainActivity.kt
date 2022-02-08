4252b89bdd64998faaff582257f3e7fc561ded25
Vladimir Vetrov <pdastore1@gmail.com>
2022-02-08 15:41:53 +0300

    Update layout

--- app/src/main/java/com/alongo/screenovatetest/presentation/main/MainActivity.kt
+++ app/src/main/java/com/alongo/screenovatetest/presentation/main/MainActivity.kt
@@ -3,7 +3,16 @@ package com.alongo.screenovatetest.presentation.main
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.alongo.screenovatetest.R
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,7 +22,40 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(ComposeView(this).apply {
+            setContent {
+                MainScreenView(viewModel)
+            }
+        })
+
         viewModel.loadPayments()
+    }
+}
+
+@Composable
+fun MainScreenView(viewModel: MainViewModel) {
+    LazyColumn {
+        items(viewModel.payments.size) {
+            val item = viewModel.payments[it]
+            BasicCard(source = item.source, destination = item.destination, amount = item.amount)
+        }
+    }
+}
+
+@Composable
+fun BasicCard(source: String, destination: String, amount: Double) {
+    val backgroundColor = if (amount >= 20) {
+        Color.Green
+    } else {
+        Color.Red
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+    ) {
+        Text("Source: $source")
+        Text("Destination: $destination")
+        Text("Amount: $amount")
     }
 }
