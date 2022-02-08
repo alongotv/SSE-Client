3b9de57a077c573e698f3543bf231fa0fe45b966
Vladimir Vetrov <pdastore1@gmail.com>
2022-02-08 16:11:35 +0300

    Add class PaymentDtoDisplayMapperModule

---
+++ app/src/main/java/com/alongo/screenovatetest/di/data/utils/mapper/PaymentDtoDisplayMapperModule.kt
@@ -0,0 +1,18 @@
+package com.alongo.screenovatetest.di.data.utils.mapper
+
+import com.alongo.screenovatetest.domain.entity.Payment
+import com.alongo.screenovatetest.domain.entity.dto.PaymentDto
+import com.alongo.screenovatetest.utils.mapper.DtoDisplayMapper
+import com.alongo.screenovatetest.utils.mapper.payment.PaymentDtoDisplayMapper
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class PaymentDtoDisplayMapperModule {
+    @Provides
+    fun providePaymentDtoDisplayMapper(): DtoDisplayMapper<PaymentDto, Payment> =
+        PaymentDtoDisplayMapper()
+}
