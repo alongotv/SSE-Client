f525b23909d6a96d9f2201d8ecc30d71c44b8c40
Vladimir Vetrov <pdastore1@gmail.com>
2022-02-08 15:42:03 +0300

    Update MainViewModel

--- app/src/main/java/com/alongo/screenovatetest/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/alongo/screenovatetest/presentation/main/MainViewModel.kt
@@ -1,10 +1,13 @@
 package com.alongo.screenovatetest.presentation.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.alongo.screenovatetest.data.networking.payment.PaymentApi
 import com.alongo.screenovatetest.domain.entity.Payment
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @HiltViewModel
 class MainViewModel @Inject constructor(private val paymentApi: PaymentApi): ViewModel() {
@@ -12,7 +15,15 @@ class MainViewModel @Inject constructor(private val paymentApi: PaymentApi): Vie
     var payments: List<Payment> = emptyList()
 
     fun loadPayments() {
-        paymentApi
+        subscribeToPayments()
         payments = listOf(Payment("", "", 10.0), Payment("", "", 20.0), Payment("", "", 30.0))
+    }
+
+    fun subscribeToPayments() {
+        viewModelScope.launch {
+            paymentApi.getPayments().collect {
+                println(it)
+            }
+        }
     }
 }
